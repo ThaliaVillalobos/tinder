4afb8fc4d854201ffe5dc1bffdd346c9536df358
Mario Martinez <mart1041@m506-118-26441.local>
2018-03-26 15:32:18 -0700

    Card rotates as user pans

--- TinderLab/CardsViewController.swift
+++ TinderLab/CardsViewController.swift
@@ -27,12 +27,22 @@ class CardsViewController: UIViewController {
     
     @IBAction func didPanImage(_ sender: UIPanGestureRecognizer) {
         let translation = sender.translation(in: view)
+        let xFromCenter = cardView.center.x - view.center.x
         
         if sender.state == .began {
             cardInitialCenter = cardView.center
             print("Gesture began")
         } else if sender.state == .changed {
             cardView.center = CGPoint(x: cardInitialCenter.x + translation.x, y: cardInitialCenter.y )
+            if xFromCenter > 0 {
+                cardView.transform = CGAffineTransform(rotationAngle: CGFloat(Double(xFromCenter) * Double.pi / 360))
+                print(translation.x)
+            }else{
+                cardView.transform = CGAffineTransform(rotationAngle: CGFloat(Double(xFromCenter) * Double.pi / 360))
+                print(xFromCenter)
+
+            }
+            
             print("Gesture is changing")
         } else if sender.state == .ended {
             print("Gesture ended")
